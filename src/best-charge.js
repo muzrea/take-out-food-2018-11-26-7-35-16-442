ece80fb4814aecfa2eadad54cd568fd03fd85455
Muzrea <muzrea@hotmail.com>
2018-12-29 11:46:38 +0800

    Calculate two promotions.

--- src/best-charge.js
+++ src/best-charge.js
@@ -42,4 +42,54 @@ function getNondiscountSum(inputsInformation) {
   return nondiscountSum;
 }
 
+function reduction(inputsInformation, nondiscountSum) {
+  let settledItems = {};
+  settledItems.items = inputsInformation;
+  settledItems.discountType = "reductionPromotion";
+  settledItems.discountSum = 6;
+  settledItems.allSum = nondiscountSum - settledItems.discountSum;
+  return settledItems;
+}
+
+function getSum(discountItems) {
+  for (let item of discountItems) {
+    item.sum = item.price * item.number;
+  }
+  return discountItems;
+}
+
+
+function productDiscount(inputsInformation, nondiscountSum, promotions) {
+  let settledItems = {};
+  let discountItems = [];
+  let i = 0;
+  let itemDiscount = promotions[1].items;
+  for (let item of inputsInformation) {
+    discountItems[i] = {};
+    discountItems[i].id = item.id;
+    discountItems[i].name = item.name;
+    discountItems[i].number = item.number;
+    settledItems.discountProduct = [];
+    if (itemDiscount.indexOf(item.id) > -1) {
+      discountItems[i].price = item.price / 2;
+      // settledItems.discountProduct[i] = item.name;
+    } else {
+      discountItems[i].price = item.price;
+    }
+    i++;
+  }
+  console.log(discountItems);
+  discountItems = getSum(discountItems);
+  for (let element of inputsInformation) {
+    if (itemDiscount.indexOf(element.id) > -1) {
+      settledItems.discountProduct.push(element.name);
+    }
+  }
+  settledItems.allSum = getNondiscountSum(discountItems);
+  settledItems.discountSum = nondiscountSum - settledItems.allSum;
+  settledItems.items = inputsInformation;
+  settledItems.discountType = "productPromotion";
+  return settledItems;
+}
+
 module.exports = bestCharge;
