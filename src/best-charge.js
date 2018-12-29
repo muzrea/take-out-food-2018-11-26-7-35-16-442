92b5af1caf34de6bd0da6e4cbd5576192c8b401a
Muzrea <muzrea@hotmail.com>
2018-12-29 11:23:40 +0800

    Require modules.

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,11 @@
+'use strict';
+const loadAllItems = require('./items.js');
+const loadPromotions = require('./promotions.js');
+let allItems = loadAllItems();
+let promotions = loadPromotions();
+
 function bestCharge(selectedItems) {
   return /*TODO*/;
 }
+
+module.exports = bestCharge;
